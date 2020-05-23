6b1ac5e6e5eca0097ef84fc8d6c3d1c81cdda0fa
CarnunMP <carnun@hotmail.co.uk>
2020-05-23 13:12:57 +0100

    Made a start on makeMove function.

--- src/toybox/TicTacToe.jsx
+++ src/toybox/TicTacToe.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import styled from 'styled-components';
 
-const StyledTicTacToe = styled.div`
+const StyledGrid = styled.div`
   width: 200px;
   height: 200px;
-  background: grey;
+  background: black;
   margin: 1rem auto;
 
   display: flex;
@@ -14,35 +14,87 @@ const StyledTicTacToe = styled.div`
 
   .row {
     width: 100%;
-    flex-grow: 1;
+    height: 33.3%;
 
     display: flex;
     flex-direction: row;
 
     .square {
       border: 2px solid white;
-      flex-grow: 1;
+      width: 33.3%;
+      height: 100%;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      p {
+        width: 1rem;
+        height: 1rem;
+        color: white;
+      }
     }
   }
+`;
 
+const StyledStartButton = styled.button`
+  width: 80px;
+  height: 35px;
+  margin: 1rem auto;
+  background: black;
+  color: white;
+  
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default function TicTacToe(props) {
+  // instantiate grid
   const newGrid = [[null, null, null],
                    [null, null, null],
                    [null, null, null]];
 
   const [grid, setGrid] = useState(newGrid);
+  const [playerIsX, setPlayerIsX] = useState(true);
+  const [playerIsMoving, setPlayerIsMoving] = useState(playerIsX); // TODO: randomise start of game
+  
+  const makeMove = (row, col) => {
+    const modifiedGrid = grid;
+
+    if (playerIsMoving) {
+      modifiedGrid[row][col] = playerIsX ? 1 : 0;
+    } else {
+      // AI move
+      modifiedGrid[row][col] = playerIsX ? 0 : 1;
+    }
+
+    setPlayerIsMoving(!playerIsMoving);
+    setGrid(modifiedGrid);
+    console.log(grid);
+  };
+
+  // const startNewGame = () => {
+  //   // flip coin to determine who starts, player or computer
+  //   const playerStarts = Math.floor(Math.random() * 2) === 0;
+  // }
 
   return (
-    <StyledTicTacToe>
-      {grid.map((row, i) => (
-        <div className='row' key={i}>
-          {grid.map((col, j) => (
-            <div className='square' key={j}/>
-          ))}
-        </div>
-      ))}
-    </StyledTicTacToe>
+    <>
+    <StyledGrid>
+    {grid.map((row, i) => (
+      <div className='row' key={i}>
+        {row.map((col, j) => (
+          <div className='square' key={j} onClick={() => makeMove(i, j)}>
+            {col === 1 ? <p>X</p> : col === 0 ? <p>O</p> : null}
+          </div>
+        ))}
+      </div>
+    ))}
+    </StyledGrid>
+    {/* <StyledStartButton onClick={startNewGame}>
+      Start
+    </StyledStartButton> */}
+    </>
   )
 }
